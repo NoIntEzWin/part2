96db25df7d5fb0d9fd0e1c9fd9915fb2e1bd21f0
NoIntEzWin <raresrary58@gmail.com>
2023-10-30 19:01:51 +0200

    Initial commit

---
+++ src/ex1/Dog.java
@@ -0,0 +1,35 @@
+package ex1;
+import java.util.Scanner;
+
+public class Dog {
+
+        public class Dog {
+            private string nume;
+            private string rasa;
+        }
+
+        public Dog(){
+            {
+                public Dog(String nune, String rasa)
+                {
+                    this nume = nume;
+                    this rasa = rasa;
+                }
+
+                public String getName() {
+                    retunr nume;
+                }
+                public void setName(String nume){
+                    this.nume = nume;
+                }
+
+                public String getBreed{
+                    return rasa;
+                }
+
+                public void setBreed(String rasa){
+                    this.rasa = rasa;
+                }
+            }
+        }
+    }
